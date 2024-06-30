01cc5eac234dbe017c62082ff366f280752fb632
Pham Van Duong <83070538+frey-eth@users.noreply.github.com>
2024-07-01 00:07:46 +0700

    hehe

--- app/[comic]/read/page.tsx
+++ app/[comic]/read/page.tsx
@@ -5,9 +5,11 @@ import { FaRegEye } from "react-icons/fa";
 import read_data from "@constants/demo-data.json";
 import { IoChevronBackOutline } from "react-icons/io5";
 import { useRouter } from "next/navigation";
+import { useInView } from "react-intersection-observer";
 
 const ReadPage = () => {
   const router = useRouter();
+
   return (
     <div className="w-full pt-[90px] px-5 flex flex-col gap-4 items-center">
       <div
@@ -19,31 +21,32 @@ const ReadPage = () => {
           {read_data.data.manga.name}
         </span>
       </div>
-      <div className=" max-w-[900px] max-md:w-full flex flex-col items-center gap-1 justify-center">
-        <div className="w-full flex flex-row h-[200px] gap-2 p-3 shadow-lg rounded-lg">
-          <div className="h-full w-[160px]">
+      <div className="max-w-[900px] max-md:w-full flex flex-col items-center gap-1 justify-center">
+        <div className="w-full flex flex-row max-md:flex-col md:h-[200px] gap-2 p-3 shadow-lg rounded-lg">
+          <div className="h-full w-full md:w-[160px] flex-1">
             <img
               className="w-full h-full object-cover rounded-lg"
               src={read_data.data.manga.panorama_url}
+              alt={read_data.data.manga.name}
             />
           </div>
-          <div className="flex flex-row items-end">
+          <div className="flex flex-row items-end flex-2">
             <div className="flex flex-col gap-1">
-              <div className="text-2xl font-bold">
+              <div className="text-2xl max-md:text-md font-bold">
                 {read_data.data.manga.name}
               </div>
-              <div className="text-md text-gray-500">
+              <div className="text-md max-md:text-sm text-gray-500">
                 {read_data.data.manga.description}
               </div>
 
               <div className="flex flex-row max-md:gap-2 gap-3 items-center">
-                <div className=" text-yellow-400 flex flex-row items-center gap-1">
+                <div className="text-yellow-400 flex flex-row items-center gap-1">
                   <FaStar size={18} /> <span>4.5/5</span>
                 </div>
-                <div className=" text-black flex flex-row items-center gap-1">
+                <div className="text-black flex flex-row items-center gap-1">
                   <CiBookmark size={18} /> <span>321</span>
                 </div>
-                <div className=" text-black flex flex-row items-center gap-1">
+                <div className="text-black flex flex-row items-center gap-1">
                   <FaRegEye size={18} /> <span>1.2k</span>
                 </div>
               </div>
@@ -51,9 +54,27 @@ const ReadPage = () => {
           </div>
         </div>
         {read_data.data.pages.map((img, index) => (
-          <img src={img.image_url} className="w-full" key={index} />
+          <LazyLoadImage key={index} src={img.image_url} />
         ))}
       </div>
+    </div>
+  );
+};
+
+const LazyLoadImage = ({ src }: { src: string }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: "50px 0px",
+  });
+
+  return (
+    <div ref={ref} className="w-full relative">
+      {inView && (
+        <>
+          <FaRegEye className="absolute top-2 right-2 text-white text-xl" />
+          <img src={src} className="w-full" alt="Manga page" />
+        </>
+      )}
     </div>
   );
 };
